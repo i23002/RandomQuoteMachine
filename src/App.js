28d164477f24090ec38d7ee77561604104d3b4d1
i23002 <indradeep.halder23@gmail.com>
2022-10-26 11:46:04 +0530

    changing colors is almost finish

--- src/App.js
+++ src/App.js
@@ -6,14 +6,39 @@ import './App.css';
 // declare an iterator
 // eslint-disable-next-line no-unused-vars
 // declare inline styles
+const text_color = "white"
+const colors = [
+  "cyan",
+  "magenta",
+  "olivegreen",
+  "orchid",
+  "orange",
+  "red",
+  "gainsboro",
+  "gold",
+  "greenyellow",
+  "hotpink",
+  "indianred",
+  "lawngreen",
+  "lightblue",
+  "mediumspringgreen",
+  "limegreen",
+  "lightsteelblue",
+  "lightskyblue",
+  "lightsalmon",
+  "mistyrose"
+]
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       i : 0,
+      j : 0,
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      backgroundColor: '',
+      color : ''
     };
     this.Increment = this.Increment.bind(this);
   }
@@ -42,10 +67,12 @@ class App extends React.Component {
   // handle change component
   Increment(){
     this.setState(state => ({
-      i: state.i + 1
+      i: state.i + 1,
+      backgroundColor : colors[state.j],
+      j : state.j + 1,
+      color : text_color
     }));
   }
-    
   
 
   render() {
@@ -56,19 +83,28 @@ class App extends React.Component {
       return <div>Loading...</div>;
     } else if(items[this.state.i].author === null){
       items[this.state.i].author = 'Unknown'
+      const styles = {
+        color: "black",
+        backgroundColor : this.state.backgroundColor
+      }
       return(
-        <div id="quote_box">
+        <div id="quote_box" style={styles}>
           <h1 id="text">{items[this.state.i].text}</h1>
           <h2 id="author">-{items[this.state.i].author}</h2>
-          <button onClick={this.Increment} id="new-quote">Click Here</button>
+          <button onClick={this.Increment} id="new-quote" style={styles}>Click Here</button>
         </div>
+        
       )
     }
      else {
+      const styles = {
+        color: "black",
+        backgroundColor : this.state.backgroundColor
+      }
       return (
-        <div id="quote_box">
-          <h1 id="text">{items[this.state.i].text}</h1>
-          <h2 id="author">-{items[this.state.i].author}</h2>
+        <div id="quote_box" style={styles}>
+          <h1 id="text"><u>{items[this.state.i].text}</u></h1>
+          <h2 id="author"><u>-{items[this.state.i].author}</u></h2>
           <button onClick={this.Increment} id="new-quote">Click Here</button>
         </div>
       );
